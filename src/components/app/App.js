6a98e0556ef453a5a0e0c2a0a5abe93595fada75
Lex <Lexor.V.A.S@yandex.ru>
2023-04-20 00:08:32 +0300

    init project

---
+++ src/components/app/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { MainPage } from "../main-page/main-page";
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <MainPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/main-page/main-page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./main-page.module.css";
+
+
+
+export const MainPage = () => {
+  return (
+    <main className={styles.main}>
+    
+    </main>
+  );
+};
