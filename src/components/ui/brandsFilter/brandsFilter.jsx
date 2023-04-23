db6a92bbe666dfa61510c7eb43da031206f313da
Lex <Lexor.V.A.S@yandex.ru>
2023-04-23 19:08:56 +0300

    fix brandsFilter

--- src/components/ui/brandsFilter/brandsFilter.jsx
+++ src/components/ui/brandsFilter/brandsFilter.jsx
@@ -1,17 +1,24 @@
-import React from "react"
-
+import React, { useState } from "react"
 import styles from "./brandsFilter.module.css"
 import brands from "../../constans/mockData/brands.json"
 
 export const BrandsFilter = () => {
-
+    const [checked, setChecked] = useState(true);
+    const [arrBrands, setArrBrands] = useState(brands.map((brand => {  return {isCheked:false, ...brand}})))
+    console.log(arrBrands)
     return (
         <fieldset className={styles.main}>
             <h3>Бренды</h3>
-            {brands.map((item) => {
+            {arrBrands.map((item) => {
                 return(
                     <div>
-            <input className={styles.checkbox} type="checkbox" id={item.title} name={item.title} value="K" />
+            <input className={styles.checkbox}
+              type="checkbox"
+              id={item.title}
+              name={item.title} 
+              value={item.title} 
+              defaultChecked={checked} 
+            onChange={() => setChecked(!checked)} />
             <label for={item.title}>{item.title}</label>
                     </div>
                 )
